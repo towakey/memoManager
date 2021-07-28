b99cda394cf5194497cba6db6989447aead6f150
towakey <kyosgame@gmail.com>
2021-07-29 00:46:51 +0900

    redcarpetを追加 編集用edit.rbを作成、入力機能はまだ

--- edit.rb
+++ edit.rb
@@ -4,3 +4,19 @@ require 'json'
 cgi = CGI.new "html5"
 
 url = cgi['json']
+hash = File.open(url) do |file|
+    JSON.load(file)
+end
+
+cgi.out do
+    cgi.html do
+        cgi.head {
+            cgi.title {"EDIT"}
+        } +
+        cgi.body {
+            cgi.h1 {"EDIT[" + url + "]"} +
+            cgi.hr +
+            cgi.p {hash["contents"]}
+        }
+    end
+end
--- index.rb
+++ index.rb
@@ -1,10 +1,22 @@
+require 'bundler/setup'
 require 'cgi'
 require 'json'
+require 'redcarpet'
+require 'redcarpet/render_strip'
 
 url = "memo.json"
 hash = File.open(url) do |file|
     JSON.load(file)
 end
+
+markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+    autolink: true,
+    tables: true,
+    fenced_code_blocks: true,
+    highlight: true
+)
+
+html = markdown.render(hash["contents"])
 
 cgi = CGI.new "html5"
 
@@ -14,11 +26,10 @@ cgi.out do
             cgi.title {"memoManager"}
         } +
         cgi.body {
-            cgi.h1 {"memoManager"} +
+            "<a href='edit?json=#{url}'>EDIT</a>" +
             cgi.hr +
-            # cgi.a({href: "edit?json=#{url}"}) +
-            cgi.hr +
-            cgi.p {hash["contents"]}
+            # cgi.p {hash["contents"]}
+            cgi.p {html}
         }
     end
 end
